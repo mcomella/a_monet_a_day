a8f77d5bfcb801a37e981011d55e6ba430160d5b
Michael Comella <michael.l.comella@gmail.com>
2024-12-29 15:03:27 -0800

    improve output of pre-push hook.

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-set -e
+# Display npm test output only on error. This is necessary because jasmine only outputs to stdout so we can't just mute
+# stdout.
+echo -n "pre-push: executing unit tests…"
+TEST_OUTPUT="$(npm test)"
+TEST_EXIT=$?
+if [ $TEST_EXIT -ne 0 ]
+then
+    echo -e " FAILED.\n$TEST_OUTPUT"
+    exit $TEST_EXIT
+fi
+echo " success."
 
-npm test
+set -e
